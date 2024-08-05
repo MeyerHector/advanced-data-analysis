d33ff27148c88e83909f6e06d35488b460641a0b
MeyerHector <meyer-hector@hotmail.com>
2024-08-05 15:47:00 -0300

    Feat: importacion de datos

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import psycopg2
-
+import pandas as pd
+data = 'employeeData.csv'
+df = pd.read_csv(data)
+print(df.head())
 try:
     # Conectar a la base de datos PostgreSQL
     connection = psycopg2.connect(
@@ -14,19 +17,32 @@ try:
     
     # Ejecutar una consulta
     create_table_query = '''
-    DROP TABLE IF EXISTS employeeperformance;
-    
-    CREATE TABLE EmployeePerformance (
-        id SERIAL PRIMARY KEY,
-        employee_id INTEGER NOT NULL,
-        department TEXT NOT NULL,
-        performance_score DECIMAL(2, 1) NOT NULL,
-        years_with_company INTEGER NOT NULL,
-        salary DECIMAL(10, 2) NOT NULL
-    );
+        DROP TABLE IF EXISTS EmployeePerformance;
+
+        CREATE TABLE EmployeePerformance (
+            id SERIAL PRIMARY KEY,
+            employee_id INTEGER NOT NULL,
+            department TEXT NOT NULL,
+            performance_score DECIMAL(3, 1) NOT NULL,
+            years_with_company INTEGER NOT NULL,
+            salary DECIMAL(6, 2) NOT NULL
+        );
     '''
     cursor.execute(create_table_query)
     
+    for index, row in df.iterrows():
+        insert_query = '''
+        INSERT INTO EmployeePerformance (employee_id, department, performance_score, years_with_company, salary)
+        VALUES (%s, %s, %s, %s, %s)
+        '''
+        cursor.execute(insert_query, (
+            row['employee_id'],
+            row['department'],
+            row['performance_score'],
+            row['years_with_company'],
+            row['salary']
+        ))
+
     # Obtener el resultado
     connection.commit()
 
